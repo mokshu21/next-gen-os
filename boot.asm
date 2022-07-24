483d1db224584bd3a1ae595ab140e98004a3c136
mokshu21 <mokshaupadhyaya@gmail.com>
2022-07-24 13:12:50 +0530

    chnages after interrupt vector table

--- boot.asm
+++ boot.asm
@@ -2,11 +2,25 @@ ORG 0
 BITS 16
 ._start:
     jmp short start
+times 33 db 0
 start:
     jmp 0x7c0:step2
-    nop
-times 33 db 0
+handle_zero:
+    mov ah,0eh
+    mov al,'A'
+    mov bx,0x00
+    int 0x10
+    iret
+handle_one:
+    mov ah,0eh
+    mov al,'V'
+    int 0x10
+    iret
 step2:
+    mov word[ss:0x00],handle_zero
+    mov word[ss:0x02],0x7c0
+    mov word[ss:0x04],handle_one
+    mov word[ss:0x06],0x7c0
     cli ;clear interrupts
     mov ax,0x7c0
     mov ds,ax
---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+nasm -f bin ./boot.asm -o ./boot.bin
+qemu-system-x86_64 -hda ./boot.bin
